e9bccc31b2f54993433c939b50d2c505f491f24e
caopan <caopan45@live.com>
2022-04-11 22:03:13 +0800

    cp modify 1

--- src/test/java/org/SleepSort.java
+++ src/test/java/org/SleepSort.java
@@ -2,6 +2,7 @@ package org;
 
 public class SleepSort {
     public static void main(String[] args) {
+        //存在问题，如果数据相差不大的情况存在错误的情况
         int [] arr = new int[]{99,45,423,3,21,6,7,8,9,10,11,12,13,14,15,16,17,18,19,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,};
         for(int i = 0 ; i < arr.length; i++){
             final int finalI = arr[i];
